766528ff6ad38395ddd0550f219dfbadd33680dc
cybergarant <62794239+cybergarant@users.noreply.github.com>
2020-04-08 05:46:15 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-test
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <title>Рейтинг пользователей</title>
+  </head>
+  <body>
+    <h1>USERS</h1>
+  </body>
+</html>
